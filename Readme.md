db5696d8967fbc059db689e3bb1e888a507d8f03
Johan Otterud <johan.otterud@gmail.com>
2020-07-06 23:18:37 +0200

    First version!

--- Readme.md
+++ Readme.md
@@ -1,5 +1,100 @@
-# useExpander
+# use-expander
 
-_Tiny React hooks for creating expanders (with CSS animation support)_
+_Tiny React hooks for creating expanders (with regular CSS animation support!)_
 
-`TODO`
+## What?
+
+React hooks for creating expander (accordion etc) components.
+
+Takes care of all the hard parts like how to animate between `0` and `auto`, `overflow`, accessibility (`TODO`) and so on.
+
+> For more context about this read this great article: https://css-tricks.com/using-css-transitions-auto-dimensions/
+
+## Why?
+
+- Very easy to use!
+
+- Existing solutions felt way to old, complicated and large. This is a super small library that only takes care of the essentials and lets you do the rest.
+
+- All implementations are done using only hooks and modern "React code" and does not rely on things like timers etc.
+
+- No built in animation implementation (this is a good thing), you are free to implement the transitions using regular `.css` files, libraries like emotion or regular React inline styles.
+
+- Accessibility support `TODO`
+
+- Full TypeScript support.
+
+## How to use
+
+Using css files
+
+```css
+/* In your .css file */
+.expander-animation {
+  transition: height 0.2s ease-out;
+}
+```
+
+```tsx
+import { useExpander } from "use-expander";
+
+export const Expander = () => {
+  // Start collapsed
+  const { toggleExpanded, contentProps } = useExpander<HTMLDivElement>(false);
+
+  return (
+    <div>
+      <button onClick={() => toggleExpanded()}>Expand content</button>
+      <div className="expander-animation" {...contentProps}>
+        This is my content
+      </div>
+    </div>
+  );
+};
+```
+
+Using inline styles:
+
+```tsx
+import { useExpander } from "use-expander";
+
+export const Expander = () => {
+  // Start collapsed
+  const { toggleExpanded, contentProps } = useExpander<HTMLDivElement>(false);
+
+  return (
+    <div>
+      <button onClick={() => toggleExpanded()}>Expand content</button>
+      <div
+        ref={contentProps.ref}
+        style={{ ...contentProps.style, transition: "height 0.2s ease-out" }}
+      >
+        This is my content
+      </div>
+    </div>
+  );
+};
+```
+
+"Controlled" mode (you handle and store the state of expanded/collapsed):
+
+```tsx
+import { useExpander } from "use-expander";
+
+export const Expander = () => {
+  const [expanded, setExpanded] = useState(false);
+  const { contentProps } = useControlledExpander<HTMLDivElement>(expanded);
+
+  return (
+    <div>
+      <button onClick={() => setExpanded(!expanded)}>Expand content</button>
+      <div
+        ref={contentProps.ref}
+        style={{ ...contentProps.style, transition: "height 0.2s ease-out" }}
+      >
+        This is my content
+      </div>
+    </div>
+  );
+};
+```
--- src/useExpander.ts
+++ src/useExpander.ts
@@ -1,24 +1,105 @@
 import { useState, useRef, useEffect } from "react";
 
-export const useExpander = <TContentElement extends HTMLElement>(defaultExpanded: boolean) => {
+// Thank you: https://css-tricks.com/using-css-transitions-auto-dimensions/
+
+export const useExpander = <TContentElement extends HTMLElement>(
+  defaultExpanded: boolean
+) => {
   const [expanded, setExpanded] = useState(defaultExpanded);
   const expander = useControlledExpander<TContentElement>(expanded);
 
-  return { ...expander, setExpanded: setExpanded };
+  return {
+    ...expander,
+    expanded: expanded,
+    setExpanded: setExpanded,
+    toggleExpanded: () => setExpanded(!expanded),
+  };
 };
 
-export const useControlledExpander = <TContentElement extends HTMLElement>(expanded?: boolean) => {
-  const [contentHeight, setContentHeight] = useState<number | undefined>(undefined);
-  const contentRef = useRef<TContentElement | null>(null);
+export const useControlledExpander = <TContentElement extends HTMLElement>(
+  expanded?: boolean
+) => {
+  const contentRef = useRef<TContentElement>(null);
+  const animationState = useRef<
+    "expanded" | "expanding" | "collapsing" | "collapsed"
+  >(expanded ? "expanded" : "collapsed");
+
+  const [contentHeight, setContentHeight] = useState<number | string>(
+    expanded ? "auto" : 0
+  );
+
+  const collapseContent = () => {
+    if (
+      (animationState.current === "expanded" ||
+        animationState.current === "expanding") &&
+      contentRef.current
+    ) {
+      animationState.current = "collapsing";
+
+      const element = contentRef.current;
+      const currentContentHeight = element.scrollHeight;
+
+      requestAnimationFrame(() => {
+        // Before animating back to height 0 we need to give it a "real height" value (we can't go from auto -> 0), we also need to give
+        // the browser a chance to apply this change before doing the animation
+        setContentHeight(currentContentHeight);
+
+        requestAnimationFrame(() => {
+          setContentHeight(0);
+          animationState.current = "collapsed";
+        });
+      });
+    } else {
+      //console.log("Nothing to do (collapsing content)");
+    }
+  };
+
+  const expandContent = () => {
+    if (
+      (animationState.current === "collapsed" ||
+        animationState.current === "collapsing") &&
+      contentRef.current
+    ) {
+      animationState.current = "expanding";
+      const element = contentRef.current;
+      const sectionHeight = element.scrollHeight;
+
+      setContentHeight(sectionHeight);
+
+      const handleTransitionEnd = () => {
+        // Clean up after ourselves
+        element.removeEventListener("transitionend", handleTransitionEnd);
+
+        if (animationState.current === "expanding") {
+          // The animation has finished lets set the height to auto
+          setContentHeight("auto");
+          animationState.current = "expanded";
+        } else {
+          // The animation state is no longer "expanding", this can happen if we try to collapse before the animation
+          // has finished, in that case do nothing
+        }
+      };
+
+      // TODO: Will this leak if the DOM element is unmounted before it has a chance to run? I guess it's ok in modern browsers but older ones?
+      // This will run when the transition animation finished for the expanding
+      element.addEventListener("transitionend", handleTransitionEnd);
+    } else {
+      //console.log("Nothing to do (expand content)");
+    }
+  };
 
   useEffect(() => {
-    if (contentRef.current) {
-      setContentHeight(contentRef.current.scrollHeight);
+    if (expanded) {
+      expandContent();
+    } else {
+      collapseContent();
     }
-  }, []);
+  }, [expanded]);
 
   return {
-    expanded: expanded,
-    contentProps: { ref: contentRef, style: { maxHeight: expanded ? contentHeight : 0 } },
+    contentProps: {
+      ref: contentRef,
+      style: { overflow: "hidden", height: contentHeight },
+    },
   };
 };
